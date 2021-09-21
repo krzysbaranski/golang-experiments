4a640275bd6abc78db3e871ccc1ca84ddc302391
Krzysztof Baranski <baranski5@gmail.com>
2021-09-21 14:35:58 +0200

    log, err

--- hello/hello.go
+++ hello/hello.go
@@ -2,12 +2,17 @@ package main
 
 import (
     "fmt"
-
+    "log"
     "github.com/krzysbaranski/golang-experiments/greetings"
 )
 
 func main() {
+    log.SetPrefix("greetings: ")
+    log.SetFlags(0)
     // Get a greeting message and print it.
-    message := greetings.Hello("Gladys")
+    message, err := greetings.Hello("Gladys")
+    if err != nil {
+        log.Fatal(err)
+    }
     fmt.Println(message)
 }

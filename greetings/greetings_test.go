94bd10041fdea11efa0fbd6134f9c108cc6b4165
Krzysztof Baranski <baranski5@gmail.com>
2021-09-22 17:36:23 +0200

    test

---
+++ greetings/greetings_test.go
@@ -0,0 +1,24 @@
+package greetings
+
+import (
+	"regexp"
+	"testing"
+)
+
+
+func TestHelloName(t *testing.T) {
+	name := "Krzys"
+	want := regexp.MustCompile(`\b`+name+`\b`)
+	msg, err := Hello("Krzys")
+	if !want.MatchString(msg) || err != nil {
+		t.Fatalf(`Hello("Krzys") = %q, %v, want match for %#q, nil`, msg, err, want)
+	}
+}
+func TestHelloEmpty(t *testing.T) {
+	var s string = ""
+	msg, err := Hello(s)
+	if msg != "" || err == nil {
+		t.Fatalf(`Hello("") = %q, %v, want "", error`, msg, err)
+	}
+
+}
